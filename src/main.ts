598159441b0d9ece5d7cbb96eb832c26c554207a
Sergio Cerda <sergio.cerda.lozano@gmail.com>
2024-04-14 04:06:26 -0400

    Seccion 27 - 381

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
     whitelist: true,
@@ -12,6 +14,6 @@ async function bootstrap() {
     })
    );
 
-  await app.listen(3000);
+  await app.listen( process.env.PORT ?? 3000 );
 }
 bootstrap();
